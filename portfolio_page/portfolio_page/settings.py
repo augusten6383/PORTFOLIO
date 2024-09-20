a4c558957b3c88feb1de8d58d72d4ff68b6434e3
augusten6383 <augustenrabi322@gmail.com>
2024-09-20 11:09:30 +0530

    new try

--- portfolio_page/portfolio_page/settings.py
+++ portfolio_page/portfolio_page/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9u2&q(vv8qcg*-^8_s=sg%cu(kcckptatm-o8r3wn)nw5i*z1f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
